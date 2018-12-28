d7549e78f5fb6f2066661017c3561eb751fafdc6
Hayden Sharkey <haydensharkey@Haydens-MacBook-Pro.local>
2018-12-27 21:50:25 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,29 @@
 
 
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+var cards = ["queen" , "queen" , "king" , "king"];
 
-console.log("User flipped" + cardTwo);
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+
+console.log("User flipped Queen");
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped king");
+
+if (cardsInPlay === 2) {
+	console.log("Two Cards in Play");
+} else if (cardsInPlay[0] === cardsInPlay[2]) {
+	alert("You found a match");
+} else if (cardsInPlay[0] !== cardsInPlay[2]) {
+	alert("Sorry try again");
+};
+
+
+
+
+
+
